e170caa055ae0e7d70664e0a420681b783229957
Yuri Castro <castroyuri09@gmail.com>
2020-10-13 19:03:25 -0500

    fixed search bar + topic displayed

--- app.py
+++ app.py
@@ -16,8 +16,21 @@ def index():
                         message = "No Topic Input"
         return render_template("index.html", message=message)
 
-@app.route('/<name>')
+# @app.route('/search', methods=['post', 'get'])
+# def index():
+#         message = ''
+#         if request.method == 'POST':
+#                 topic = request.form.get('topic')  # access the data inside
+#                 return redirect(topic)
+#                 # render_template(f"/{username}")
+#         return render_template("articles.html")
+
+@app.route('/<name>',  methods=['post', 'get'])
 def choose_article(name):
+        if request.method == 'POST':
+                topic = request.form.get('topic')  # access the data inside
+                return redirect(topic)
+                # render_template(f"/{username}")
         newsapi = NewsApiClient(api_key='be667b6da1734020a35b7c340f255a28')
         all_articles = newsapi.get_everything(q=name)
         return render_template("articles.html", topic=name, articles=all_articles['articles'])
